baadedc363216483e37406437fd612c37555a6c0
safe-ending <safe_ending@163.com>
2021-08-17 11:28:41 +0800

    Delete myscript directory

--- myscript/src/main/java/com/myscript/iink/uireferenceimplementation/ISizeListener.java
+++
@@ -1,8 +0,0 @@
-// Copyright @ MyScript. All rights reserved.
-
-package com.myscript.iink.uireferenceimplementation;
-
-public interface ISizeListener
-{
-  void sizeChanged(int newWidth, int newHeight);
-}
