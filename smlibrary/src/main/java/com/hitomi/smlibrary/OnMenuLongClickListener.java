37cf7026c87b8d3d218195064f10d925ada5e38e
safe-ending <safe_ending@163.com>
2021-08-17 11:29:12 +0800

    Delete smlibrary directory

--- smlibrary/src/main/java/com/hitomi/smlibrary/OnMenuLongClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.hitomi.smlibrary;
-
-/**
- * 点击中间位置菜单的回调接口
- * Created by hitomi on 2016/9/19.
- */
-public interface OnMenuLongClickListener {
-    void onMenuLongClick(int position);
-}
