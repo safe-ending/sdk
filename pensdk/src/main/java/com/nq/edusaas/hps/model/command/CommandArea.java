740077babbdc1d7b1641065555e44cd26364f4fa
safe-ending <safe_ending@163.com>
2021-08-17 11:28:25 +0800

    Delete pensdk directory

--- pensdk/src/main/java/com/nq/edusaas/hps/model/command/CommandArea.java
+++
@@ -1,8 +0,0 @@
-package com.nq.edusaas.hps.model.command;
-
-public class CommandArea extends CommandBase {
-    public CommandArea(int code) {
-        super.setCode(code);
-        super.setType(COMMAND_TYPE_AREA);
-    }
-}
